77dc8b2e59e22291e4d3bb0e5cbb4d71c4e258b6
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-05-07 20:46:34 -0500

    changed the layout of the app to sidebar

--- views/New.jsx
+++ views/New.jsx
@@ -3,31 +3,129 @@ const React = require("react");
 class New extends React.Component {
   render() {
     return (
-      <div>
-        <h1>New page</h1>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 
-        <form action="/users" method="POST">
-          First Name: <input type="text" name="firstName" />
-          <br />
-          LastName: <input type="text" name="lastName" />
-          <br />
-          Email: <input type="email" name="email" />
-          <br />
-          Phone: <input type="text" name="phone" />
-          <br />
-          Department: <input type="text" name="department" />
-          <br />
-          Job Title: <input type="text" name="jobTitle" />
-          <br />
-          Job Description: <input type="text" name="jobDescription" />
-          <br />
-          Created on: <input type="date" name="createdOn" />
-          <br />
-          Is active: <input type="checkbox" name="isActive" />
-          <br />
-          <input type="submit" name="" value="Create User" />
-        </form>
-      </div>
+          {/* Main StyleSheet */}
+          <link rel="stylesheet" href="/css/style.css" />
+
+          {/* FontAwesome */}
+
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+            integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+            crossOrigin="anonymous"
+          />
+          <title>Index Page</title>
+        </head>
+        <body>
+          <div className="wrapper">
+            <div className="sidebar">
+              <div className="logo">
+                <img
+                  src="https://res.cloudinary.com/duprwuo4j/image/upload/v1574831158/imgs_starwars/imgs/BLOGO_k36v5y.png"
+                  alt="logo"
+                />
+              </div>
+              <ul>
+                <li>
+                  <a href="http://localhost:3001/users">
+                    <i className="fas fa-home"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://localhost:3001/users/new">
+                    <i className="fas fa-plus"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://localhost:3001/users">
+                    <i className="fas fa-users"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://www.bruno-dasilva.com/">
+                    <i className="fas fa-cookie-bite"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="main_content">
+              <div class="header">
+                <div className="title">
+                  <h1>Active Directory App</h1>
+                </div>
+                <div className="social-icons">
+                  <div className="logo">
+                    <img
+                      src="https://res.cloudinary.com/duprwuo4j/image/upload/v1588899368/Logo/blogo-gray_doyoy3.png"
+                      alt="logo"
+                    />
+                  </div>
+                  <div className="info-Bruno">
+                    <a
+                      href="https://github.com/DaSilvaBrunoTexas"
+                      target="_blank"
+                    >
+                      <i className="fab fa-github-alt"></i>
+                    </a>
+
+                    <a
+                      href="https://www.linkedin.com/in/bruno-dasilva/"
+                      target="_blank"
+                    >
+                      <i class="fab fa-linkedin-in"></i>
+                    </a>
+
+                    <a href="http://bruno-dasilva.com/" target="_blank">
+                      <i class="fas fa-globe-americas"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="info">
+                <div>
+                  <h1>New page</h1>
+                  <div className="container">
+                    <form action="/users" method="POST">
+                      First Name: <input type="text" name="firstName" />
+                      <br />
+                      LastName: <input type="text" name="lastName" />
+                      <br />
+                      Email: <input type="email" name="email" />
+                      <br />
+                      Phone: <input type="text" name="phone" />
+                      <br />
+                      Department: <input type="text" name="department" />
+                      <br />
+                      Job Title: <input type="text" name="jobTitle" />
+                      <br />
+                      Job Description:{" "}
+                      <input type="text" name="jobDescription" />
+                      <br />
+                      Created on: <input type="date" name="createdOn" />
+                      <br />
+                      Is active: <input type="checkbox" name="isActive" />
+                      <br />
+                      <input type="submit" name="" value="Create User" />
+                    </form>
+                    <div>
+                      <img src="" alt="Add User Form" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>
     );
   }
 }
--- views/index.jsx
+++ views/index.jsx
@@ -30,8 +30,14 @@ class Index extends React.Component {
           <title>Index Page</title>
         </head>
         <body>
-          <div className="container">
-            <div className="nav-bar">
+          <div className="wrapper">
+            <div className="sidebar">
+              <div className="logo">
+                <img
+                  src="https://res.cloudinary.com/duprwuo4j/image/upload/v1574831158/imgs_starwars/imgs/BLOGO_k36v5y.png"
+                  alt="logo"
+                />
+              </div>
               <ul>
                 <li>
                   <a href="http://localhost:3001/users">
@@ -55,44 +61,70 @@ class Index extends React.Component {
                 </li>
               </ul>
             </div>
-            {users.map((user, index) => {
-              return (
-                <div class="demo__div people">
-                  <div class="people__div people__div--img">
+            <div class="main_content">
+              <div class="header">
+                <div className="title">
+                  <h1>Active Directory App</h1>
+                </div>
+                <div className="social-icons">
+                  <div className="logo">
                     <img
-                      src="https://source.unsplash.com/mEZ3PoFGs_k"
-                      alt={user.firstName}
-                      class="people__img"
+                      src="https://res.cloudinary.com/duprwuo4j/image/upload/v1588899368/Logo/blogo-gray_doyoy3.png"
+                      alt="logo"
                     />
                   </div>
-                  <div class="people__div people__div--info">
-                    <h2 class="people__name">
-                      {user.firstName} {user.lastName}
-                    </h2>
-                    <p class="people__desc">
-                      {user.description} Time waits for no man. Unless that man
-                      is Chuck Norris. When Chuck Norris jumps into a pool, he
-                      does not get wet, the water gets Chuck Norris.
-                    </p>
-                  </div>
-                  <div class="people__div people__div--name">
-                    <h2 class="people-name">{user.jobTitle}</h2>
+                  <div className="info-Bruno">
+                    <a
+                      href="https://github.com/DaSilvaBrunoTexas"
+                      target="_blank"
+                    >
+                      <i className="fab fa-github-alt"></i>
+                    </a>
+
+                    <a
+                      href="https://www.linkedin.com/in/bruno-dasilva/"
+                      target="_blank"
+                    >
+                      <i class="fab fa-linkedin-in"></i>
+                    </a>
+
+                    <a href="http://bruno-dasilva.com/" target="_blank">
+                      <i class="fas fa-globe-americas"></i>
+                    </a>
                   </div>
                 </div>
-              );
-            })}
-
-            <div></div>
+              </div>
+              <div class="info">
+                {users.map((user, index) => {
+                  return (
+                    <div class="demo__div people">
+                      <div class="people__div people__div--img">
+                        <img
+                          src="https://source.unsplash.com/mEZ3PoFGs_k"
+                          alt={user.firstName}
+                          class="people__img"
+                        />
+                      </div>
+                      <div class="people__div people__div--info">
+                        <h2 class="people__name">
+                          {user.firstName} {user.lastName}
+                        </h2>
+                        <p class="people__desc">
+                          {user.description} Time waits for no man. Unless that
+                          man is Chuck Norris. When Chuck Norris jumps into a
+                          pool, he does not get wet, the water gets Chuck
+                          Norris.
+                        </p>
+                      </div>
+                      <div class="people__div people__div--name">
+                        <h2 class="people-name">{user.jobTitle}</h2>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
-          <svg viewBox="0 0 100 25">
-            <path fill="#1f2833" d="M0 30 V12 Q30 17 55 12 T100 11 V30z"></path>
-          </svg>
-
-          <footer>
-            <p>
-              2020 Bruno DaSilva <span> Budget App</span>
-            </p>
-          </footer>
         </body>
       </html>
     );
